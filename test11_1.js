c8c7c2bcbdee86ee02e2633f09ee5dfc6d6f2032
kjm19940711 <392507275@qq.com>
2016-03-12 14:53:06 +0800

    Update test11_1.js

--- test11_1.js
+++ test11_1.js
@@ -1,2 +1,2 @@
 Bar_Chart_fun([8,2,1,4,5,9,6,3,7,4,8,5,10],"Time","Speed",400,270,120,50,d3.select("#Barchart_div"))
-paint_linechart([8,2,1,4,5,9,6,3,7,4,8,5,10],"Time","Speed",400,270,120,400,d3.select("#Barchart_div"))
+paint_linechart([8,2,1,4,5,9,6,3,7,4,8,5,10],"Time","Speed",400,270,120,350,d3.select("#LineChart"))
